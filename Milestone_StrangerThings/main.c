3bcc402c95db956107712515c08af963b20e9fa6
Lonnie Souder II <souderl9@students.rowan.edu>
2017-10-13 19:20:02 -0400

    UART savepoint

--- Milestone_StrangerThings/main.c
+++ Milestone_StrangerThings/main.c
@@ -1,5 +1,17 @@
 #include <msp430.h>
 
+#define RED BIT5
+#define GREEN BIT6
+#define BLUE BIT7
+#define BUTTON BIT5
+
+
+int hex2duty(int hex)
+{
+    float duty;
+    duty = 1000-(hex/0xFF)*1000;
+    return (int)duty;
+}
 
 unsigned int RxCount = 0;					// Keeps track of how many bytes have been received
 unsigned int nBytes = 0;					// Stores number of bytes in current packet
@@ -8,9 +20,26 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop Watchdog
 
+    P3SEL0 |= RED + GREEN + BLUE;
+    P3DIR |= RED + GREEN + BLUE;
+    P3OUT &= ~(RED + GREEN + BLUE);
+
+    TB0CTL |=   TBSSEL_2 |ID_1 | MC_1;
+
+    TB0CCR0 = 1000;              // Set clock period
+
+    TB0CCR4 = 1000;
+    TB0CCTL4 |= OUTMOD_3;       // Set/Reset mode
+
+    TB0CCR5 = 1000;
+    TB0CCTL5 |= OUTMOD_3;       // Set/Reset mode
+
+    TB0CCR6 = 1000;
+    TB0CCTL6 |= OUTMOD_3;       // Set/Reset mode
+
     // Configure GPIO
-    P2SEL0 &= ~(BIT0 | BIT1);
-    P2SEL1 |= BIT0 | BIT1;                  // USCI_A0 UART operation
+    P6SEL0 &= ~(BIT0 | BIT1);
+    P6SEL1 |= BIT0 | BIT1;                  // USCI_A0 UART operation
 
     // Disable the GPIO power-on default high-impedance mode to activate
     // previously configured port settings
@@ -24,50 +53,52 @@ int main(void)
     CSCTL0_H = 0;                           // Lock CS registers
 
     // Configure USCI_A0 for UART mode
-    UCA0CTLW0 = UCSWRST;                    // Put eUSCI in reset
-    UCA0CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
+    UCA3CTLW0 = UCSWRST;                    // Put eUSCI in reset
+    UCA3CTLW0 |= UCSSEL__SMCLK;             // CLK = SMCLK
     // Baud Rate calculation
     // 8000000/(16*9600) = 52.083
     // Fractional portion = 0.083
     // User's Guide Table 21-4: UCBRSx = 0x04
     // UCBRFx = int ( (52.083-52)*16) = 1
-    UCA0BRW = 52;                           // 8000000/16/9600
-    UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-    UCA0CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
-    UCA0IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
+    UCA3BRW = 52;                           // 8000000/16/9600
+    UCA3MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
+    UCA3CTLW0 &= ~UCSWRST;                  // Initialize eUSCI
+    UCA3IE |= UCRXIE;                       // Enable USCI_A0 RX interrupt
 
-    __bis_SR_register(LPM3_bits | GIE);     // Enter LPM3, interrupts enabled
-    __no_operation();                       // For debugger
+    __bis_SR_register(LPM0_bits | GIE);     // Enter LPM3, interrupts enabled
+    //__no_operation();                       // For debugger
 }
 
 
-#pragma vector=EUSCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
+#pragma vector=EUSCI_A3_VECTOR
+__interrupt void USCI_A3_ISR(void)
 {
-	//RxCount = 0;
 
-    switch(__even_in_range(UCA0IV, USCI_UART_UCTXCPTIFG))
+    switch(__even_in_range(UCA3IV, USCI_UART_UCTXCPTIFG))
     {
         case USCI_NONE: break;
         case USCI_UART_UCRXIFG:
-            while(!(UCA0IFG & UCTXIFG));
+            while(!(UCA3IFG & UCTXIFG));
             	if (RxCount == 0 ) {
-            		nBytes = UCA0RXBUF;
-            		UCA0TXBUF = nBytes - 3;
+            		nBytes = UCA3RXBUF;
+            		UCA3TXBUF = nBytes - 3;
             		RxCount ++;
             	}
             	else if (RxCount == 1) {	// Red info
             		RxCount ++;
+            		TB0CCR4 = hex2duty(UCA3RXBUF);
             	}
 
             	else if (RxCount == 2) {	// Green info
             		RxCount ++;
+            		TB0CCR5 = hex2duty(UCA3RXBUF);
             	}
             	else if (RxCount == 3) {	// Blue info
             		RxCount ++;
+            		TB0CCR6 = hex2duty(UCA3RXBUF);
             	}
             	else if (RxCount > 2) {		// Pass these along
-            		UCA0TXBUF = UCA0RXBUF;
+            		UCA3TXBUF = UCA3RXBUF;
             		RxCount ++;
             		if (RxCount == nBytes)	// EOM
             			RxCount = 0;
